ef33f7b1812b91eb5b8af443d47cc8b05d698da3
Loris <45939615+lchastanet@users.noreply.github.com>
2023-08-01 14:48:25 +0200

    remove debug

--- backend/src/middlewares/validateSchema.js
+++ backend/src/middlewares/validateSchema.js
@@ -5,12 +5,6 @@ const validateSchema = (schema) => [
   (req, res, next) => {
     const errors = validationResult(req)
 
-    console.log("===================")
-
-    console.log(JSON.stringify(errors))
-
-    console.log("===================")
-
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.mapped() })
     }
